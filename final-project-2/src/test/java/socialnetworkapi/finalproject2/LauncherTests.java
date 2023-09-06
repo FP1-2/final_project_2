6ccfcc9709b65ef657829e8cd491e2391d97f725
Alex <sashaep61@gmail.com>
2023-09-06 21:10:56 +0300

    --spring initial

---
+++ final-project-2/src/test/java/socialnetworkapi/finalproject2/LauncherTests.java
@@ -0,0 +1,13 @@
+package socialnetworkapi.finalproject2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LauncherTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
