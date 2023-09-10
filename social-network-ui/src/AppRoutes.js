b6f758adb6445c5e20df27ea804ed8976cc380cd
Slava Tiutiunnyk <gloryon87@gmail.com>
2023-09-10 09:26:25 +0300

    added header from material

--- social-network-ui/src/AppRoutes.js
+++ social-network-ui/src/AppRoutes.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
+import Header from './components/header'
 
 const AppRoutes = () => {
 	return (
+		<>
+		<Header/>
 		<Routes>
 			{/* Public Routes */}
 			<Route path='/' element={<HomePage />} />
@@ -15,6 +18,7 @@ const AppRoutes = () => {
 			{/* Error Path */}
 			<Route path='*' element={<h1>error</h1>} />
 		</Routes>
+		</>
 	)
 }
 
