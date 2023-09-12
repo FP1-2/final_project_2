cecd9335b62e2510cf35a330575acfb613f850db
ArsLoD <arslod123@gmail.com>
2023-09-12 17:20:15 +0300

    add scdstep of signup, add pretty visual

--- social-network-ui/src/AppRoutes.js
+++ social-network-ui/src/AppRoutes.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import HomePage from './pages/HomePage'
+// import HomePage from './pages/HomePage'
 import AuthPage from './pages/AuthPage'
 import LoginPage from './pages/LoginPage'
 
