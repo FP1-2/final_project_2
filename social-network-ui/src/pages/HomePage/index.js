8d7904150933e3f7a079fd6d282face347943cc4
Bohdan Admin <bohdanadm1n@gmail.com>
2023-09-13 20:12:16 +0300

    Revert "Header"

--- social-network-ui/src/pages/HomePage/index.js
+++ social-network-ui/src/pages/HomePage/index.js
@@ -1,14 +1,10 @@
 import React from 'react'
-import Header from '../../components/header'
 
 const Home = () => {
 	return (
-		<>
-		<Header pageName='Main'/>
 		<div>
 			<h1>home</h1>
 		</div>
-		</>
 	)
 }
 
