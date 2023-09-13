2e0fbd4bad1107c67d0f227b3e5ad46aea38cf48
evgeniygalagurya <evgeniygalagurya@gmail.com>
2023-09-13 11:50:12 +0300

    ver2.3

--- social-network-api/src/main/java/fs/socialnetworkapi/controller/RegistrationController.java
+++ social-network-api/src/main/java/fs/socialnetworkapi/controller/RegistrationController.java
@@ -1,41 +1,41 @@
 package fs.socialnetworkapi.controller;
 
-
 import fs.socialnetworkapi.entity.User;
 import fs.socialnetworkapi.service.UserService;
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
-
-import java.util.Map;
-
-
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class RegistrationController {
   @Autowired
   private UserService userService;
 
-  @GetMapping("/registration")
-  public String registration() {
-    return "registration";
-  }
+  //  @GetMapping("/registration")
+  //  public String registration() {
+  //    return "registration";
+  //  }
+
+  //  @PostMapping("/registration")
+  //  public String addUser(User user, Map<String, Object> model) {
+  //    if (!userService.addUser(user)) {
+  //      model.put("message", "User exists!");
+  //      return "registration";
+  //    }
+  //    return "activation";
+  //  }
 
   @PostMapping("/registration")
-  public String addUser(User user, Map<String, Object> model) {
-    if (!userService.addUser(user)) {
-      model.put("message", "User exists!");
-      return "registration";
-    }
-
-    return "activation";
+  public void create(@RequestBody User user) {
+    userService.addUser(user);
   }
+
 
   @GetMapping("/activate/{code}")
   public String activate(Model model, @PathVariable String code) {
@@ -46,7 +46,6 @@ public class RegistrationController {
     } else {
       model.addAttribute("message", "Activation code is not found!");
     }
-
     return "login";
   }
 }
