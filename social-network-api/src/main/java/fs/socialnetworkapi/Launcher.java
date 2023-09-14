b66a32e0ccb7b6f1c2370024c048cc8351cac6cd
evgeniygalagurya <evgeniygalagurya@gmail.com>
2023-09-14 11:12:20 +0300

    version2.0

--- social-network-api/src/main/java/fs/socialnetworkapi/Launcher.java
+++ social-network-api/src/main/java/fs/socialnetworkapi/Launcher.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 
 @SpringBootApplication
 public class Launcher {
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) {
     SpringApplication.run(Launcher.class, args);
   }
 
