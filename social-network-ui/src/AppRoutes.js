587c7956429524393498f4ff5bf183cf8135e77f
Slava Tiutiunnyk <gloryon87@gmail.com>
2023-09-14 17:05:46 +0300

    changed header component to using redux toolkit

--- social-network-ui/src/AppRoutes.js
+++ social-network-ui/src/AppRoutes.js
@@ -1,24 +1,30 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-// import HomePage from './pages/HomePage/HomePage.js'
+import HomePage from './pages/HomePage/HomePage'
 import AuthPage from './pages/AuthPage/AuthPage'
 import LoginPage from './pages/LoginPage/LoginPage'
 
 const AppRoutes = () => {
-	return (
-		<Routes>
-			{/* Public Routes */}
-			<Route path='/' element={<AuthPage />} />
+  return (
+    <Routes>
+      {/* Public Routes */}
+      <Route path='/' element={<AuthPage />} />
+      <Route path='/home' element={<HomePage />} />
+      <Route path='/explore' element={<HomePage />} />
 
-			{/* Auth Routes */}
-			<Route path='/signIn' element={<LoginPage />} />
+      {/* Auth Routes */}
+      <Route path='/signIn' element={<LoginPage />} />
 
-			{/* Profile Routes */}
+      {/* Profile Routes */}
+      <Route path='/notifications' element={<HomePage />} />
+      <Route path='/messages' element={<HomePage />} />
+      <Route path='/bookmarks' element={<HomePage />} />
+      <Route path='/profile' element={<HomePage />} />
 
-			{/* Error Path */}
-			<Route path='*' element={<h1>error</h1>} />
-		</Routes>
-	)
+      {/* Error Path */}
+      <Route path='*' element={<h1>error</h1>} />
+    </Routes>
+  )
 }
 
 export default AppRoutes
