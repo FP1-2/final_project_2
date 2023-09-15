56630155839ab525514578ede5c731f0c6f1e1d2
Yehor Chervonyi <tatuman4ik@gmail.com>
2023-09-15 13:06:24 +0300

    Us13 PasswordController PasswordResetService need to make Upgrade User Func

--- social-network-api/src/main/java/fs/socialnetworkapi/controller/RegistrationController.java
+++ social-network-api/src/main/java/fs/socialnetworkapi/controller/RegistrationController.java
@@ -5,34 +5,30 @@ import fs.socialnetworkapi.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequiredArgsConstructor
 public class RegistrationController {
-  @Autowired
-  private UserService userService;
+    @Autowired
+    private UserService userService;
 
-  @PostMapping("/registration")
-  public void create(@RequestBody User user) {
-    userService.addUser(user);
-  }
-
-
-  @GetMapping("/activate/{code}")
-  public String activate(Model model, @PathVariable String code) {
-    boolean isActivated = userService.activateUser(code);
-
-    if (isActivated) {
-      model.addAttribute("message", "User successfully activated");
-    } else {
-      model.addAttribute("message", "Activation code is not found!");
+    @PostMapping("/registration")
+    public void create(@RequestBody User user) {
+        userService.addUser(user);
     }
-    return "login";
-  }
+
+
+    @GetMapping("/activate/{code}")
+    public String activate(Model model, @PathVariable String code) {
+        boolean isActivated = userService.activateUser(code);
+
+        if (isActivated) {
+            model.addAttribute("message", "User successfully activated");
+        } else {
+            model.addAttribute("message", "Activation code is not found!");
+        }
+        return "login";
+    }
 }
