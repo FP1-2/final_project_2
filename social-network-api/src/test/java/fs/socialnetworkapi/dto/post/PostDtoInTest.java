c877a88a5d29434a3f8111625f0ddd1cc4f838af
Alex <sashaep61@gmail.com>
2023-09-20 00:14:39 +0300

    --US17 Post controller

--- social-network-api/src/test/java/fs/socialnetworkapi/dto/post/PostDtoInTest.java
+++ social-network-api/src/test/java/fs/socialnetworkapi/dto/post/PostDtoInTest.java
@@ -6,36 +6,49 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PostDtoInTest {
 
+  PostDtoIn postDtoIn = new PostDtoIn(1L,1L,"photo","description");
   @Test
   void getId() {
+    assertEquals(1L, postDtoIn.getId());
   }
 
   @Test
   void getUserId() {
+    assertEquals(1L, postDtoIn.getUserId());
   }
 
   @Test
   void getPhoto() {
+    assertEquals("photo", postDtoIn.getPhoto());
   }
 
   @Test
   void getDescription() {
+    assertEquals("description", postDtoIn.getDescription());
   }
 
   @Test
   void setId() {
+    postDtoIn.setId(2L);
+    assertEquals(2L, postDtoIn.getId());
   }
 
   @Test
   void setUserId() {
+    postDtoIn.setUserId(2L);
+    assertEquals(2L, postDtoIn.getUserId());
   }
 
   @Test
   void setPhoto() {
+    postDtoIn.setPhoto("photo2");
+    assertEquals("photo2", postDtoIn.getPhoto());
   }
 
   @Test
   void setDescription() {
+    postDtoIn.setDescription("description2");
+    assertEquals("description2", postDtoIn.getDescription());
   }
 
   @Test
@@ -44,6 +57,7 @@ class PostDtoInTest {
 
   @Test
   void canEqual() {
+
   }
 
   @Test
