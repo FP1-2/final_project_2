482fd306251f39649747622e12dbeda76d9b94f7
Alex <sashaep61@gmail.com>
2023-09-20 12:27:45 +0300

    --US17 Post controller

--- social-network-api/src/main/java/fs/socialnetworkapi/dto/post/PostDtoOut.java
+++ social-network-api/src/main/java/fs/socialnetworkapi/dto/post/PostDtoOut.java
@@ -1,6 +1,10 @@
 package fs.socialnetworkapi.dto.post;
 
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Builder;
 
 import java.util.Objects;
 
