01eab183a8b316144a141208071813d9b05d4cc4
Yehor Chervonyi <tatuman4ik@gmail.com>
2023-10-02 12:14:16 +0300

    Likes Style check error

--- social-network-api/src/main/java/fs/socialnetworkapi/controller/LikeController.java
+++ social-network-api/src/main/java/fs/socialnetworkapi/controller/LikeController.java
@@ -5,7 +5,12 @@ import fs.socialnetworkapi.service.LikeService;
 import fs.socialnetworkapi.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
