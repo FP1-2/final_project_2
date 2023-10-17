768e25c6d626f29fc5d67965124305cc9e3a9c8a
Yehor Chervonyi <tatuman4ik@gmail.com>
2023-10-17 16:34:53 +0300

    Notifications

--- social-network-api/src/main/java/fs/socialnetworkapi/controller/NotificationController.java
+++ social-network-api/src/main/java/fs/socialnetworkapi/controller/NotificationController.java
@@ -5,7 +5,11 @@ import fs.socialnetworkapi.service.NotificationService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
