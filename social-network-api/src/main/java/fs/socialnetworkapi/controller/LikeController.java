be92293d8de15c0eca57651fd5b99bf2023f0d7e
Yehor Chervonyi <tatuman4ik@gmail.com>
2023-10-19 17:14:22 +0300

    Some changes

--- social-network-api/src/main/java/fs/socialnetworkapi/controller/LikeController.java
+++ social-network-api/src/main/java/fs/socialnetworkapi/controller/LikeController.java
@@ -4,7 +4,11 @@ import fs.socialnetworkapi.dto.post.PostDtoOut;
 import fs.socialnetworkapi.service.LikeService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
