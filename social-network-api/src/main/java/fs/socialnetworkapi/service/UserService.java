1868900be179987cc0bc867ec277902c670efbe6
EvgeniyGalagurya <112659308+EvgeniyGalagurya@users.noreply.github.com>
2023-10-21 10:13:09 +0300

    Update UserService.java

--- social-network-api/src/main/java/fs/socialnetworkapi/service/UserService.java
+++ social-network-api/src/main/java/fs/socialnetworkapi/service/UserService.java
@@ -58,7 +58,7 @@ public class UserService implements UserDetailsService {
     user.setUserFollowersCount(getFollowers(userId).size());
     user.setUserTweetCount(getUserPosts(userId, 0, 1000000).size());// need to correct
 
-    return mapper.map(userRepo.save(user), UserDtoOut.class);
+    return mapper.map(user, UserDtoOut.class);
   }
 
   public List<PostDtoOut> getUserPosts(Long currentUserId, Integer page, Integer size) {
