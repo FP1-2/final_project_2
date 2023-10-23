57759e78d873532c15eb7f28c95e65139cb1f7da
Albina11 <albinabasova1994@gmail.com>
2023-10-23 23:49:41 +0300

    upd-mess

--- social-network-ui/src/components/Chats/Chats.jsx
+++ social-network-ui/src/components/Chats/Chats.jsx
@@ -1,7 +1,23 @@
 import React,{useState} from 'react'
 import styles from './Chats.module.scss';
 function Chats() {
-
+  const userId = {
+	id: 50,
+	username: 'Camel_wafas',
+	firstName: 'Jack',
+	lastName: 'Petrov',
+	email: 'asda@gmail.com',
+	birthday: '1992-04-04',
+	avatar: null,
+	address: 'Poltava',
+	createdDate: '2023-10-16T10:59:39.842808',
+	userDescribe: 'I am a good person',
+	userLink: 't.me/adadsa',
+	bgProfileImage: null,
+	userTweetCount: 55,
+	userFollowersCount: 12,
+	userFollowingCount: 34,
+}
      const [chats, setChats] = useState([])
   return (
       <div className={styles.chatsContainer}>
@@ -11,8 +27,8 @@ function Chats() {
   <div className={styles.allUsersChat} style={{ cursor: "pointer" }}  onClick={() => {}}>
     <div style={{ backgroundImage: `url: avatar` }} className={styles.profilePic}></div>
     <div className={styles.chatHeadingCon}>
-      <h2>.userInfo.username</h2>
-      <span>.userInfo?.usersAt</span>
+      <h2>{userId.firstName}</h2>
+      <span>{userId.avatar}</span>
       <p>.lastMessage?.texts?...</p>
     </div>
   </div>
--- social-network-ui/src/pages/MessagePage/MessagePage.jsx
+++ social-network-ui/src/pages/MessagePage/MessagePage.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import Header from "../../components/Header/header";
-import { Box, CssBaseline } from "@mui/material";
+import { Avatar, Box, CssBaseline } from "@mui/material";
 import Chats from '../../components/Chats/Chats';
 import Chat from '../../components/Chat/Chat';
 import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
@@ -8,7 +8,7 @@ import MailOutlinedIcon from '@mui/icons-material/MailOutlined';
 import classNames from 'classnames'
 import styles from './MessagePage.module.scss';
 import { Link } from "react-router-dom";
-
+import axios from 'axios';
 
 
 
@@ -18,21 +18,57 @@ function MessagePage() {
   const [err, setErr] = useState(false)
   const [chatComponentActive, setChatComponentActive] = useState(false)
 	const [isMobile, setIsMobile] = useState(false)
+
+
+   const handleSearch = async () => {
+    try {
+      const response = await axios.get (`http://twitterdanit.us-east-1.elasticbeanstalk.com/api/v1/user/info/1`);
+      if (response === 200) {
+        const data = await response.json();
+        setUser(data);
+        setErr(false);
+      } else {
+        setErr(true);
+      }
+    } catch (error) {
+      setErr(true);
+    }
+  }
+
+
+  const userId = {
+	id: 50,
+	username: 'Camel_wafas',
+	firstName: 'Jack',
+	lastName: 'Petrov',
+	email: 'asda@gmail.com',
+	birthday: '1992-04-04',
+	avatar: null,
+	address: 'Poltava',
+	createdDate: '2023-10-16T10:59:39.842808',
+	userDescribe: 'I am a good person',
+	userLink: 't.me/adadsa',
+	bgProfileImage: null,
+	userTweetCount: 55,
+	userFollowersCount: 12,
+	userFollowingCount: 34,
+}
+
   return (
   <Header >
     <Box sx={{
       position: 'relative',
       zIndex: 1300,
     }}>
-{/* <div className={classNames[isMobile ? 'ChatActive' : "messagesContainer"]} > */}
+<div className={classNames({ ChatActive: isMobile, messagesContainer: !isMobile })}>
 <div className={classNames(styles.messagesContainer)} >
    
       <div className={classNames(styles.centerGridContainer)} >
         <header>
           <h1>Messages </h1>
           <div className={styles.messageIcon} >
-            <span>{<SettingsOutlinedIcon fontSize='30px' opacity={0.3} />} </span>
-            <span>{<MailOutlinedIcon  fontSize='30px' opacity={0.3} />} </span>
+            <span>{<SettingsOutlinedIcon fontSize='30px' />} </span>
+            <span>{<MailOutlinedIcon  fontSize='30px' />} </span>
           </div>
         </header>
         <div className={styles.inputContainer} >
@@ -40,15 +76,17 @@ function MessagePage() {
         
           </div>
           {err && <p style={{ marginTop: 20}} > Error USER NOT FOUND</p> }
-          {user && <div onClick={handleSelect } className={styles.userChat}>
-            <div style={{ backgroundImage: `url(${user.avatar})` }} className={styles.profilePic} > </div>
+          {userId && <div  className={styles.userChat}>
+            <div style={{ backgroundImage: `url(${userId.avatar})` }}  >
+              <Avatar />
+               </div>
             <div>
-              <span>{user?.username} </span>
-              <span>{user?.usersAt} </span>
+              <span>{userId.firstName} </span>
+              <span>{userId.username} </span>
               <span></span>
             </div>
           </div>}
-            <Chats />
+            <Chats isMobile={isMobile} setIsMobile={setIsMobile} setChatComponentActive={setChatComponentActive}/>
           </div>
         <div className={styles.leftGrid} >
            {chatComponentActive ? <div ><Chat isMobile={isMobile} setIsMobile={setIsMobile} /></div> :
@@ -61,7 +99,7 @@ function MessagePage() {
                 </div>
             </div>}
         </div>
-         
+         </div>
       </div>
     </Box>
     
