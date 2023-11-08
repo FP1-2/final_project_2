9b8558dbd8475ea6bbdccbe66a77e7c5fc8bfe0f
Slava Tiutiunnyk <gloryon87@gmail.com>
2023-11-08 23:01:25 +0200

    fixed reference issue

--- social-network-ui/src/components/Favourites/favourites.jsx
+++ social-network-ui/src/components/Favourites/favourites.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import getLikedPosts from '../../api/getLikedPosts'
 import useIsAuthenticated from '../../hooks/useIsAuthenticated'
 import CircularProgress from '@mui/material/CircularProgress'
