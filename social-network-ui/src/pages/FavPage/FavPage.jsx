ad6a778fe8120ab95e2e13632a73411c7dd0bb16
Slava Tiutiunnyk <gloryon87@gmail.com>
2023-11-08 22:45:55 +0200

    fixed reference issue

--- social-network-ui/src/pages/FavPage/FavPage.jsx
+++ social-network-ui/src/pages/FavPage/FavPage.jsx
@@ -1,11 +1,8 @@
 import React from 'react'
-import Header from '../../components/Header/header'
 import Favourites from '../../components/Favourites/Favourites'
-import './FavPage.scss'
 
 const FavPage = () => {
   return (
-    // <Header pageName='Favourites'> <Favourites/> </Header>
     <Favourites/>
   )
 }
